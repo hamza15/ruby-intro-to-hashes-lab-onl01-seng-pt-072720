e590cfcd1f2df9c08bb993fb10579853271c0d4f
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-03 20:25:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,10 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names =>{
-	      :reading_railroad=>{
+	      :reading_railroad =>{
+	        
+	      },
+	      :pennsylvania_railroad => {
 	        
 	      },
 	      
